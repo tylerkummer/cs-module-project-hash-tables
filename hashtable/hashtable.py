08eca8050f4602b03286df03a9aba845d3561416
Tyler Kummer <tylerkummer@protonmail.com>
2020-08-03 14:17:53 -0500

    Initial Commit

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -2,6 +2,7 @@ class HashTableEntry:
     """
     Linked List hash table key/value pair
     """
+
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -23,7 +24,6 @@ class HashTable:
     def __init__(self, capacity):
         # Your code here
 
-
     def get_num_slots(self):
         """
         Return the length of the list you're using to hold the hash
@@ -36,7 +36,6 @@ class HashTable:
         """
         # Your code here
 
-
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
@@ -44,7 +43,6 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
 
     def fnv1(self, key):
         """
@@ -55,7 +53,6 @@ class HashTable:
 
         # Your code here
 
-
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
@@ -64,13 +61,12 @@ class HashTable:
         """
         # Your code here
 
-
     def hash_index(self, key):
         """
         Take an arbitrary key and return a valid integer index
         between within the storage capacity of the hash table.
         """
-        #return self.fnv1(key) % self.capacity
+        # return self.fnv1(key) % self.capacity
         return self.djb2(key) % self.capacity
 
     def put(self, key, value):
@@ -83,7 +79,6 @@ class HashTable:
         """
         # Your code here
 
-
     def delete(self, key):
         """
         Remove the value stored with the given key.
@@ -93,7 +88,6 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
 
     def get(self, key):
         """
@@ -105,7 +99,6 @@ class HashTable:
         """
         # Your code here
 
-
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
@@ -114,7 +107,6 @@ class HashTable:
         Implement this.
         """
         # Your code here
-
 
 
 if __name__ == "__main__":
